a3fd0fdd8c8a59327018e395580de81a7a98ace6
Luiz Farias <63025468+luizfe00@users.noreply.github.com>
2024-09-25 07:35:20 -0300

    allow origin all

--- src/configs/setup-middleware.ts
+++ src/configs/setup-middleware.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express';
-import { bodyParser, contentType, cors, authMiddleware } from '@/middlewares';
+import { bodyParser, contentType, authMiddleware } from '@/middlewares';
+import cors from 'cors'
 
 export function setupMiddleware(app: Express): void {
-  app.use(cors);
+  app.use(cors({ origin: '*' }));
   app.use(bodyParser);
   app.use(contentType);
   app.use(authMiddleware);
