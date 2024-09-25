52775b0a712c25b2d4177d986e712d33608dbd44
Luiz Farias <63025468+luizfe00@users.noreply.github.com>
2024-09-25 07:57:05 -0300

    allowing ngrok url

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 
+const allowedOrigins = [
+  'https://tcc-frontend-virid.vercel.app', // Vercel frontend
+  'https://9a2f-192-241-141-182.ngrok-free.app', // dynamic ngrok URL
+];
+
 export function cors(req: Request, res: Response, next: NextFunction): void {
-  res.set('Access-Control-Allow-Origin', 'https://tcc-frontend-virid.vercel.app');
+  const origin = req.headers.origin;
+
+  // Check if the request's origin is allowed
+  if (origin && allowedOrigins.includes(origin)) {
+    res.set('Access-Control-Allow-Origin', origin); // Dynamically set the allowed origin
+  }
+  
   res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.set(
     'Access-Control-Allow-Headers',
