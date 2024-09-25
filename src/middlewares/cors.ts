c67c650fe62b4d813f4f74341da4667b6f01f9f7
Luiz Farias <63025468+luizfe00@users.noreply.github.com>
2024-09-25 08:02:39 -0300

    removing credentials header

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,26 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
-const allowedOrigins = [
-  'https://tcc-frontend-virid.vercel.app', // Vercel frontend
-  'https://9a2f-192-241-141-182.ngrok-free.app', // dynamic ngrok URL
-];
-
 export function cors(req: Request, res: Response, next: NextFunction): void {
-  const origin = req.headers.origin;
-
-  console.log('origin', origin)
-
-  // Check if the request's origin is allowed
-  if (origin && allowedOrigins.includes(origin)) {
-    res.set('Access-Control-Allow-Origin', origin); // Dynamically set the allowed origin
-  }
-
+  res.set('Access-Control-Allow-Origin', 'https://tcc-frontend-virid.vercel.app');
   res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.set(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
-  res.set('Access-Control-Allow-Credentials', 'true');
+  // res.set('Access-Control-Allow-Credentials', 'true');
 
   // Handle preflight (OPTIONS) requests
   if (req.method === 'OPTIONS') {
