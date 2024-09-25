f57456bdc40e9599095cd37d9896bea439f2c96b
Luiz Farias <63025468+luizfe00@users.noreply.github.com>
2024-09-25 16:22:50 -0300

    cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,7 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 
+const allowedOrigins = [
+  'https://tcc-frontend-virid.vercel.app', // Vercel production
+  'http://localhost:3000', // Local development
+  'https://2ddb-192-241-141-182.ngrok-free.app' // Update this with your current ngrok URL
+];
+
 export function cors(req: Request, res: Response, next: NextFunction): void {
-  res.set('Access-Control-Allow-Origin', 'https://tcc-frontend-virid.vercel.app');
+  const origin = req.headers.origin as string;
+  console.log(origin)
+
+  if (allowedOrigins.includes(origin)) {
+    res.set('Access-Control-Allow-Origin', origin);
+  }
+
   res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.set(
     'Access-Control-Allow-Headers',
